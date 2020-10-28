4fafd4480eda8a1f344cef7ce3b80a87f2976afe
eunjoo jung <lynn0506@snu.ac.kr>
2020-10-28 17:20:04 +0900

    backend auth and user App merged

--- backend/naengpa/auth/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-
-# Create your views here.
-def signup(request):
-    return HttpResponse('signup page')
-
-def login(reqeust):
-    return HttpResponse('login page')
--- backend/naengpa/user/views.py
+++ backend/naengpa/user/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
+def signup(request):
+    return JsonResponse([], safe=False)
+ 
+def login(request):
+    return JsonResponse([], safe=False)
+
 def user_list(request):
     return JsonResponse([], safe=False)
 
