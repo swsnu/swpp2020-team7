cc5d8bc43009b24ed4f30d95548e14df96eac191
eunjoo jung <lynn0506@snu.ac.kr>
2020-11-09 04:21:45 +0900

    trying to solve 'no-self-argument' pylint error

--- backend/naengpa/user/models.py
+++ backend/naengpa/user/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+# pylint: disable-msg=R0201
+# pylint: disable=no-self-use
+
+
 class Profile(models.Model):
     ''' Profile model '''
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,12 +15,12 @@ class Profile(models.Model):
     date_of_birth = models.CharField(max_length=30)
 
     @receiver(post_save, sender=User)
-    def create_user_profile(self, sender, instance, created, **kwargs):
+    def create_user_profile(sender, instance, created, **kwargs):
         ''' create user profile '''
         if created:
             Profile.objects.create(user=instance)
 
     @receiver(post_save, sender=User)
-    def save_user_profile(self, sender, instance, **kwargs):
+    def save_user_profile(ender, instance, **kwargs):
         ''' save user profile '''
         instance.profile.save()
