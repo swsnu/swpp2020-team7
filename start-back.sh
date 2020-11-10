16f65929a68a487e478787a6890471b1d4c5b731
Eunsung Lim <unsungvegetable@gmail.com>
2020-11-10 16:12:27 +0900

    enhanced argument check

--- start-back.sh
+++ start-back.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # usage: ./start-back.sh {path-to-virtual-environment-folder}
-source $1/bin/activate
+if [ -z "$1" ] 
+then
+    echo "No virtual environment provided"
+else
+    echo "try activating virtual environment [@$1/bin/activate]"
+    source $1/bin/activate
+fi
+
 cd backend/naengpa
 pip install -r requirements.txt 
 python manage.py makemigrations
