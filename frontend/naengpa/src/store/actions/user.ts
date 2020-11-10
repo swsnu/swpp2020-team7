d41e7252929817e139280bec63fafb590df00eba
Eunsung Lim <unsungvegetable@gmail.com>
2020-11-11 00:31:15 +0900

    user management with redux

--- frontend/naengpa/src/store/actions/user.ts
+++ frontend/naengpa/src/store/actions/user.ts
@@ -1,35 +1,37 @@
 import axios from 'axios';
+import { push } from 'connected-react-router';
 import * as actionTypes from './actionTypes';
 import { UserEntity, UserLoginInputDTO, UserSignupInputDTO } from '../../model/user';
+
 /* CSRF TOKEN */
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 /* SIGNUP */
-export function signup(user: UserSignupInputDTO) {
+export const signup_ = (user: UserEntity) => ({ type: actionTypes.SIGNUP, user });
+
+export const signup = (user: UserSignupInputDTO) => {
 	return async (dispatch: any) => {
-		const response: any = await axios.post('/api/signup/', user);
+		const response = await axios.post('/api/signup/', user);
 		const currentUser: UserEntity = response.data;
 
-		dispatch({
-			type: actionTypes.SIGNUP,
-			user: currentUser,
-		});
+		dispatch(signup_(currentUser));
+		dispatch(push('/fridge'));
 	};
-}
+};
 
 /* LOGIN */
-export function login(user: UserLoginInputDTO) {
+export const login_ = (user: UserEntity) => ({ type: actionTypes.LOGIN, user });
+
+export const login = (user: UserLoginInputDTO) => {
 	return async (dispatch: any) => {
-		const response: any = await axios.post('/api/login/', user);
+		const response = await axios.post('/api/login/', user);
 		const currentUser: UserEntity = response.data;
 
-		dispatch({
-			type: actionTypes.LOGIN,
-			user: currentUser,
-		});
+		dispatch(login_(currentUser));
+		dispatch(push('/fridge'));
 	};
-}
+};
 
 /* LOGOUT */
 export function logout() {
@@ -42,7 +44,6 @@ export function logout() {
 				type: actionTypes.LOGOUT,
 			});
 		}
-		
 	};
 }
 
