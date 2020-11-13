803ad65a26d275a43b90e6b5369fc174322b1ce9
eunjoo jung <lynn0506@snu.ac.kr>
2020-11-14 06:19:25 +0900

    mock createObjectURL for testcode

--- frontend/naengpa/src/setupTests.ts
+++ frontend/naengpa/src/setupTests.ts
@@ -3,8 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-
 import { configure } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+if (typeof window.URL.createObjectURL === 'undefined') {
+	Object.defineProperty(window.URL, 'createObjectURL', {
+		value: () => {
+			console.log('empty');
+		},
+	});
+}
 configure({ adapter: new EnzymeAdapter(), disableLifecycleMethods: true });
