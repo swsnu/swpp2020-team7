d4eeabfd56b4cd9627803ffcdb52ed3bb292be1a
Eunsung Lim <unsungvegetable@gmail.com>
2020-11-15 01:24:50 +0900

    add shell script for testing backend

---
+++ test-back.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# usage: ./test-back.sh {path-to-virtual-environment-folder}
+if [ -z "$1" ] 
+then
+    echo "No virtual environment provided"
+else
+    echo "try activating virtual environment [@$1/bin/activate]"
+    source $1/bin/activate
+fi
+
+cd backend/naengpa 
+pip install -r requirements.txt 
+python manage.py makemigrations 
+python manage.py migrate
+coverage run --source='.' manage.py test && coverage report
+coverage run --branch --source='.' manage.py test && coverage report
