df5e01611d1587bb11e3b710224fed28189cce4a
eunjoo jung <lynn0506@snu.ac.kr>
2020-11-14 17:43:10 +0900

    recipe&ingredient test code complete, typo update

--- backend/naengpa/ingredient/tests.py
+++ backend/naengpa/ingredient/tests.py
@@ -1,4 +1,25 @@
 """tests for ingredient"""
 # from django.test import TestCase
-
+from django.test import TestCase, Client
+import json
+from .models import Ingredient
 # Create your tests here.
+
+
+class IngredientTestCase(TestCase):
+    def test_ingredient_list(self):
+        client = Client()
+        response = client.get('/api/ingredients/',
+                              content_type='application/json')
+        # user is not defined
+        self.assertEqual(response.status_code, 401)
+        response = client.post('/api/signup/', json.dumps({'username': 'nimo', 'name': "nimo", 'password': "nimo", 'dateOfBirth': "19950506", "email": "dori@dori.com"}),
+                               content_type='application/json')
+        response = client.get('/api/ingredients/',
+                              content_type='application/json')
+        # get ingredient list done
+        self.assertEqual(response.status_code, 200)
+        response = client.put('/api/ingredients/',
+                              content_type='application/json')
+        # bad requeset
+        self.assertEqual(response.status_code, 405)
