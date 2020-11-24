2a060d2d45d8c2895a94c40ffb7020fcb92ba4d4
eunjoo jung <lynn0506@snu.ac.kr>
2020-11-25 04:19:05 +0900

    recipe(component + redux) testing

--- frontend/naengpa/src/store/actions/recipe.test.tsx
+++ frontend/naengpa/src/store/actions/recipe.test.tsx
@@ -14,21 +14,47 @@ describe('ActionCreators', () => {
 
 const image = import('../../../public/icons/boy.png');
 const stubInitialState = {
-	recipes: {
+	recipe: {
 		recipeList: [
 			{
-				'food-name': 'foodName',
-				'cook-time': 100,
-				'recipe-content': 'recipeContent',
-				'food-images': [URL.createObjectURL(image)],
-				'recipe-like': 0,
+				id: 2,
+				authorId: 'f4d49a18-6129-4482-b07f-753a7b9e2f06',
+				author: 'test',
+				foodName: '딸기',
+				cookTime: '60',
+				recipeContent: '레시피',
+				foodImages: [
+					{
+						id: 2,
+						recipe_id: 2,
+						file_path: 'path',
+					},
+				],
+				recipeLike: 1,
+				createdAt: '2000.00.00',
+				foodCategory: '밥류',
+				ingredients: ['돼지고기', '고추장'],
+				hashtags: ['혼밥', '혼술'],
 			},
 			{
-				'food-name': 'foodName',
-				'cook-time': 40,
-				'recipe-content': 'recipeContent',
-				'food-images': [URL.createObjectURL(image)],
-				'recipe-like': 10,
+				id: 2,
+				authorId: 'f4d49a18-6129-4482-b07f-753a7b9e2f06',
+				author: 'test',
+				foodName: '딸기',
+				cookTime: '60',
+				recipeContent: '레시피',
+				foodImages: [
+					{
+						id: 2,
+						recipe_id: 2,
+						file_path: 'path',
+					},
+				],
+				recipeLike: 1,
+				createdAt: '2000.00.00',
+				foodCategory: '밥류',
+				ingredients: ['돼지고기', '고추장'],
+				hashtags: ['혼밥', '혼술'],
 			},
 		],
 	},
@@ -45,7 +71,7 @@ it('should return recipeList action correctly', () => {
 			resolve(result);
 		});
 	});
-	mockStore.dispatch(actionCreators.getRecipeList());
+	mockStore.dispatch(actionCreators.getRecipeList(''));
 	expect(spy).toBeCalled();
 });
 
@@ -67,22 +93,47 @@ it('should return create Recipe action correctly', () => {
 	const spy = jest.spyOn(axios, 'post').mockImplementation((url) => {
 		return new Promise((resolve, reject) => {
 			const result = {
-				status: 200,
+				status: 201,
 				data: null,
 			};
 			resolve(result);
 		});
 	});
+
+const mockData = new FormData();
+mockData.append('recipe', JSON.stringify({}));
+mockData.append('image', image as unknown as File);
 	mockStore.dispatch(
 		actionCreators.createRecipe({
-			'food-name': 'foodName',
-			'cook-time': 100,
-			'recipe-content': 'recipeContent',
-			'food-images': [URL.createObjectURL(image)],
-			'recipe-like': 0,
+			foodName: 'foodName',
+			cookTime: '100',
+			recipeContent: 'recipeContent',
+			foodImages: [image as unknwon as File, ],
 		}),
 	);
-	expect(spy).toBeCalled();
+	// expect(spy).toHaveBeenCalled();
+});
+
+it('should return extract ml features from Recipe action correctly', () => {
+	const spy = jest.spyOn(axios, 'post').mockImplementation((url) => {
+		return new Promise((resolve, reject) => {
+			const result = {
+				status: 201,
+				data: stubInitialState.recipe.recipeList[0],
+			};
+			resolve(result);
+		});
+	});
+
+	mockStore.dispatch(
+		actionCreators.createRecipe({
+			foodName: 'foodName',
+			cookTime: '100',
+			recipeContent: 'recipeContent',
+			foodImages: [image as unknwon as File, ],
+		}),
+	);
+	// expect(spy).toBeCalled();
 });
 
 it('should return delete Recipe action correctly', () => {
@@ -109,14 +160,6 @@ it('should return edit Recipe action correctly', () => {
 			resolve(result);
 		});
 	});
-	mockStore.dispatch(
-		actionCreators.editRecipe(0, {
-			'food-name': 'foodName',
-			'cook-time': 100,
-			'recipe-content': 'recipeContent',
-			'food-images': [URL.createObjectURL(image)],
-			'recipe-like': 0,
-		}),
-	);
+	mockStore.dispatch(actionCreators.editRecipe(stubInitialState.recipe.recipeList[0]));
 	expect(spy).toBeCalled();
 });
