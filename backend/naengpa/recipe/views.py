cb231d0c6dc6a37b4230afa5e60ce4794e790cfa
Eunsung Lim <unsungvegetable@gmail.com>
2020-11-25 23:02:12 +0900

    refactor: make s3 functions independent

--- backend/naengpa/recipe/views.py
+++ backend/naengpa/recipe/views.py
@@ -2,14 +2,14 @@
 import json
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest, HttpResponseForbidden,  HttpResponseNotFound, HttpResponseNotAllowed
 from django.views.decorators.csrf import ensure_csrf_cookie
-from naengpa.settings import S3_URL, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME
-import boto3
-from .models import Recipe, Image
-from datetime import datetime
 from django.core import serializers
+from django.db import transaction
+from utils.aws_utils import upload_images
+from .models import Recipe, Image
 
 
 @ensure_csrf_cookie
+@transaction.atomic
 def recipe_list(request):
     """get recipe list"""
     if request.method != 'GET' and request.method != 'POST':
@@ -27,7 +27,7 @@ def recipe_list(request):
         "foodName": recipe.food_name,
         "cookTime": recipe.cook_time,
         "recipeContent": recipe.recipe_content,
-        "foodImages": list(Image.objects.filter(recipe_id=recipe.id).values()),
+        "foodImages": Image.objects.filter(recipe_id=recipe.id).values()),
         "recipeLike": 0,
         "createdAt": recipe.created_at.strftime("%Y.%m.%d")
     } for recipe in sorted_list] if len(sorted_list) != 0 else []
@@ -35,58 +35,43 @@ def recipe_list(request):
     if request.user.is_authenticated:
         if request.method == 'GET':
             ''' GET /api/recipes/ get recipe list '''
-            return JsonResponse(recipe_collection, safe=False)
+            return JsonResponse(recipe_collection, safe = False)
 
         else:
             ''' POST /api/recipes/ post new recipe '''
-            req_data = eval(request.POST.dict().get('recipe', ''))
-            food_images = request.FILES.getlist('image')
-            food_name = req_data['foodName']
-            cook_time = req_data['cookTime']
-            recipe_content = req_data['recipeContent']
+            user_id=request.user.id
+            req_data=eval(request.POST.dict().get('recipe', ''))
+            food_images=request.FILES.getlist('image')
+            food_name=req_data['foodName']
+            cook_time=req_data['cookTime']
+            recipe_content=req_data['recipeContent']
 
-            recipe = Recipe.objects.create(
-                author=request.user,
-                food_name=food_name,
-                cook_time=cook_time,
-                recipe_content=recipe_content)
+            recipe=Recipe.objects.create(
+                author_id = user_id,
+                food_name = food_name,
+                cook_time = cook_time,
+                recipe_content = recipe_content)
 
-            session = boto3.Session(
-                aws_access_key_id=AWS_ACCESS_KEY_ID,
-                aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                region_name=AWS_S3_REGION_NAME
-            )
-            s3 = session.resource('s3')
+            images_path=upload_images(
+                food_images, "recipe", recipe.id, user_id)
+            for path in images_path:
+                Image.objects.create(file_path = path, recipe_id = recipe.id)
 
-            for item in food_images:
-                now = datetime.now()
-                img_object = s3.Bucket(AWS_STORAGE_BUCKET_NAME).put_object(
-                    Key="recipe/"+str(recipe.id)+"/" +
-                    str(request.user.id)+"-"+str(now),
-                    Body=item
-
-                )
-                recipe_image = S3_URL+"recipe/" + \
-                    str(recipe.id)+"/"+str(request.user.id)+"-"+str(now)
-
-                Image.objects.create(
-                    file_path=recipe_image, recipe_id=recipe.id)
-
-            return JsonResponse(data={
+            return JsonResponse(data = {
                 "id": recipe.id,
                 "authorId": recipe.author.id,
                 "author": recipe.author.username,
                 "foodName": food_name,
                 "cookTime": cook_time,
-                "foodImages": list(Image.objects.filter(recipe_id=recipe.id).values()),
+                "foodImages": Image.objects.filter(recipe_id=recipe.id).values(),
                 "recipeContent": recipe_content,
                 "recipeLike": 0,
                 "createdAt": recipe.created_at,
-            }, status=201)
+            }, status = 201)
     else:
-        return HttpResponse(status=401)
+        return HttpResponse(status = 401)
 
 
 def recipe_info(request, id):
     """get recipe of given id"""
-    return HttpResponse(status=405)
+    return HttpResponse(status = 405)
