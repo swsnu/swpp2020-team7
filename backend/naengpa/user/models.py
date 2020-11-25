c4ef8ee0bd404703ab4433988969c2a031935cdf
Eunsung Lim <unsungvegetable@gmail.com>
2020-11-26 01:58:52 +0900

    feat: add region model

--- backend/naengpa/user/models.py
+++ backend/naengpa/user/models.py
@@ -1,9 +1,17 @@
 """models for user"""
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
 from ingredient.models import Ingredient
 
 import uuid
+
+
+class Region(models.Model):
+    ''' Region model '''
+    name = models.CharField(max_length=30, unique=True)
+    location = models.PointField(
+        geography=True, default=Point(0.0, 0.0), unique=True)
 
 
 class User(AbstractUser):
@@ -13,6 +21,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=30)
     date_of_birth = models.CharField(max_length=30)
     naengpa_score = models.IntegerField(default=0)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
 
 
 class Fridge(models.Model):
