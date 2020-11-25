56b4cf8ea24ba3426da0ed4111978a12b1743e99
Eunsung Lim <unsungvegetable@gmail.com>
2020-11-25 23:03:59 +0900

    fix: fixed typo

--- backend/naengpa/recipe/views.py
+++ backend/naengpa/recipe/views.py
@@ -27,7 +27,7 @@ def recipe_list(request):
         "foodName": recipe.food_name,
         "cookTime": recipe.cook_time,
         "recipeContent": recipe.recipe_content,
-        "foodImages": Image.objects.filter(recipe_id=recipe.id).values()),
+        "foodImages": Image.objects.filter(recipe_id=recipe.id).values(),
         "recipeLike": 0,
         "createdAt": recipe.created_at.strftime("%Y.%m.%d")
     } for recipe in sorted_list] if len(sorted_list) != 0 else []
@@ -35,29 +35,29 @@ def recipe_list(request):
     if request.user.is_authenticated:
         if request.method == 'GET':
             ''' GET /api/recipes/ get recipe list '''
-            return JsonResponse(recipe_collection, safe = False)
+            return JsonResponse(recipe_collection, safe=False)
 
         else:
             ''' POST /api/recipes/ post new recipe '''
-            user_id=request.user.id
-            req_data=eval(request.POST.dict().get('recipe', ''))
-            food_images=request.FILES.getlist('image')
-            food_name=req_data['foodName']
-            cook_time=req_data['cookTime']
-            recipe_content=req_data['recipeContent']
+            user_id = request.user.id
+            req_data = eval(request.POST.dict().get('recipe', ''))
+            food_images = request.FILES.getlist('image')
+            food_name = req_data['foodName']
+            cook_time = req_data['cookTime']
+            recipe_content = req_data['recipeContent']
 
-            recipe=Recipe.objects.create(
-                author_id = user_id,
-                food_name = food_name,
-                cook_time = cook_time,
-                recipe_content = recipe_content)
+            recipe = Recipe.objects.create(
+                author_id=user_id,
+                food_name=food_name,
+                cook_time=cook_time,
+                recipe_content=recipe_content)
 
-            images_path=upload_images(
+            images_path = upload_images(
                 food_images, "recipe", recipe.id, user_id)
             for path in images_path:
-                Image.objects.create(file_path = path, recipe_id = recipe.id)
+                Image.objects.create(file_path=path, recipe_id=recipe.id)
 
-            return JsonResponse(data = {
+            return JsonResponse(data={
                 "id": recipe.id,
                 "authorId": recipe.author.id,
                 "author": recipe.author.username,
@@ -67,11 +67,11 @@ def recipe_list(request):
                 "recipeContent": recipe_content,
                 "recipeLike": 0,
                 "createdAt": recipe.created_at,
-            }, status = 201)
+            }, status=201)
     else:
-        return HttpResponse(status = 401)
+        return HttpResponse(status=401)
 
 
 def recipe_info(request, id):
     """get recipe of given id"""
-    return HttpResponse(status = 405)
+    return HttpResponse(status=405)
