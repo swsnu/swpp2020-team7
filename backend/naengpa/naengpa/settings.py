73300b838f8d24d49dac64503c9380dae9b2e1c8
Eunsung Lim <unsungvegetable@gmail.com>
2020-11-26 02:14:55 +0900

    feat: add rest-framework-gis

--- backend/naengpa/naengpa/settings.py
+++ backend/naengpa/naengpa/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'rest_framework_gis',
     'user.apps.UserConfig',
     'recipe.apps.RecipeConfig',
     'article.apps.ArticleConfig',
