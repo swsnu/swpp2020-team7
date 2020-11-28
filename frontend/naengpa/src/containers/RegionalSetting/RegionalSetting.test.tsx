46a6604c89cbce7f09e72bb0929e34e7a7f6b3d2
eunjoo jung <lynn0506@snu.ac.kr>
2020-11-28 22:28:09 +0900

    Add: Regional Setting(initial version)

--- frontend/naengpa/src/containers/RegionalSetting/RegionalSetting.test.tsx
+++ frontend/naengpa/src/containers/RegionalSetting/RegionalSetting.test.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import { history } from '../../store/store';
 import RegionalSetting from './RegionalSetting';
 
 describe('RegionalSetting', () => {
 	let regionalSetting: any;
 
 	beforeEach(() => {
-		regionalSetting = <RegionalSetting />;
+		regionalSetting = <RegionalSetting history={history} />;
 	});
 	afterEach(() => {
 		jest.clearAllMocks();
@@ -16,7 +17,21 @@ describe('RegionalSetting', () => {
 	});
 
 	it('RegionalSetting renders without crashing', () => {
-		const component = mount(regionalSetting);
-		expect(component.find('RegionalSetting').length).toBe(1);
+		const component = shallow(regionalSetting);
+		expect(component.find('#regional-setting').length).toBe(1);
+	});
+
+	it('should work well with the region search input', () => {
+		const component = shallow(regionalSetting);
+		expect(component.find('#regional-setting').length).toBe(1);
+		const regionSearchInput = component.find('#region-search-input').find('input');
+		// regionSearchInput.simulate('change', { target: { value: '봉천동' } });
+	});
+
+	it('should work well with the region confirm button', () => {
+		const component = shallow(regionalSetting);
+		expect(component.find('#regional-setting').length).toBe(1);
+		const regionSearchInput = component.find('#confirm-button');
+		regionSearchInput.simulate('click');
 	});
 });
--- frontend/naengpa/src/containers/RegionalSetting/RegionalSetting.tsx
+++ frontend/naengpa/src/containers/RegionalSetting/RegionalSetting.tsx
@@ -1,7 +1,171 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { History } from 'history';
+import { useDispatch } from 'react-redux';
 
-const RegionalSetting: React.FC = () => {
-	return <div>RegionalSetting</div>;
+import LocalDiningIcon from '@material-ui/icons/LocalDining';
+// import { signup } from '../../../store/actions/index';
+import './RegionalSetting.scss';
+import { InputBase, withStyles } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import Slider from '@material-ui/core/Slider';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+interface RegionalSettingProps {
+	history: History;
+}
+
+declare global {
+	interface Window {
+		kakao: any;
+	}
+}
+
+const PrettoSlider = withStyles({
+	root: {
+		color: '#52af77',
+		height: 8,
+	},
+	thumb: {
+		height: 24,
+		width: 24,
+		backgroundColor: '#fff',
+		border: '2px solid currentColor',
+		marginTop: -8,
+		marginLeft: -12,
+		'&:focus, &:hover, &$active': {
+			boxShadow: 'inherit',
+		},
+	},
+	active: {},
+	valueLabel: {
+		left: 'calc(-50% + 4px)',
+	},
+	track: {
+		height: 8,
+		borderRadius: 4,
+	},
+	rail: {
+		height: 8,
+		borderRadius: 4,
+	},
+})(Slider);
+
+const RegionalSetting: React.FC<RegionalSettingProps> = ({ history }) => {
+	const [query, setQuery] = useState('');
+	const [latitude, setLatitude] = useState(33.450701);
+	const [longitude, setLongitude] = useState(126.570667);
+	const [regionInfo1, setRegionInfo1] = useState('');
+	const [regionInfo2, setRegionInfo2] = useState('');
+
+	const onClickSearch = () => {
+		const mapContainer = document.getElementById('map');
+		const mapOption = {
+			center: new window.kakao.maps.LatLng(latitude, longitude),
+			level: 3,
+		};
+
+		const map = new window.kakao.maps.Map(mapContainer, mapOption);
+		const ps = new window.kakao.maps.services.Places();
+
+		const placesSearchCB = (data: any, status: any, pagination: any) => {
+			if (status === window.kakao.maps.services.Status.OK) {
+				const bounds = new window.kakao.maps.LatLngBounds();
+				data.forEach((item: any, i: any) => {
+					const bound = new window.kakao.maps.LatLng(item.y, item.x);
+					bounds.extend(bound);
+				});
+				map.setBounds(bounds);
+				const level = map.getLevel();
+				const latlng = map.getCenter();
+				displayMarker(latlng);
+			}
+		};
+
+		ps.keywordSearch(query, placesSearchCB);
+		function displayMarker(place: any) {
+			const markerPosition = new window.kakao.maps.LatLng(place.getLat(), place.getLng());
+			const marker = new window.kakao.maps.Marker({
+				map,
+				position: markerPosition,
+			});
+			marker.setMap(map);
+		}
+	};
+
+	const onClickConfirmRegion = () => {
+		setRegionInfo1('봉천동');
+	};
+
+	const marks = [
+		{
+			value: 1,
+			label: '좁음',
+		},
+		{
+			value: 3,
+			label: '중간',
+		},
+		{
+			value: 5,
+			label: '넓음',
+		},
+	];
+
+	useEffect(() => {
+		const container = document.getElementById('map');
+		const options = {
+			center: new window.kakao.maps.LatLng(latitude, longitude),
+			level: 3,
+		};
+		new window.kakao.maps.Map(container, options);
+		const mapContainer = document.getElementById('map');
+		const mapOption = {
+			center: new window.kakao.maps.LatLng(latitude, longitude),
+			level: 3,
+		};
+
+		new window.kakao.maps.Map(mapContainer, mapOption);
+	}, []);
+
+	return (
+		<div id="regional-setting">
+			<button id="naengpa" type="button" onClick={() => history.push('/fridge')}>
+				<LocalDiningIcon id="naengpa-logo" />
+				<div id="naengpa-logo-name">냉파</div>
+			</button>
+			<div id="region-part">
+				<div id="region-element-box">
+					<div id="region-part-header">지역 설정을 해보세요!</div>
+					<div id="region-part-subheader">설정한 지역의 거래만 볼 수 있어요!</div>
+					<div id="region-search-input-box">
+						<InputBase
+							id="region-search-input"
+							placeholder="동을 입력해주세요(봉천동)"
+							inputProps={{ 'aria-label': 'search' }}
+							onChange={(e) => setQuery(e.target.value)}
+							onKeyDown={onClickSearch}
+						/>
+						<SearchIcon id="region-search-icon" />
+					</div>
+					<div id="region-map">
+						<div id="map" style={{ width: '400px', height: '300px' }} />
+					</div>
+					<div id="region-setting-slider">
+						<PrettoSlider
+							defaultValue={3}
+							aria-label="pretto slider"
+							valueLabelDisplay="auto"
+							marks={marks}
+							id="slider-bar"
+						/>
+					</div>
+				</div>
+				<button id="confirm-button" type="submit" onClick={onClickConfirmRegion}>
+					CONFIRM
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default RegionalSetting;
