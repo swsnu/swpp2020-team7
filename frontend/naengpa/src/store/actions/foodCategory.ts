747279a6d2d274ab6b0b21b96caca7f51a7e7fad
snusun <0515hwang@snu.ac.kr>
2020-11-28 15:15:15 +0900

    added food category

--- frontend/naengpa/src/store/actions/foodCategory.ts
+++ frontend/naengpa/src/store/actions/foodCategory.ts
@@ -11,7 +11,21 @@ export const getFoodCategoryList = () => {
 	return async (dispatch: any) => {
 		// const response = await axios.get('/api/foodcategories/');
 		// const foodCategoryList: FoodCategoryCollection = response.data;
-		const foodCategoryList: string[] = ['밥류', '빵류', '떡류', '면류', '고기류', '라면류'];
+		const foodCategoryList: string[] = [
+			'밥류',
+			'빵류',
+			'면류',
+			'육류',
+			'해물류',
+			'생선류',
+			'계란/알류',
+			'채소류',
+			'과일류',
+			'유제품류',
+			'디저트류',
+			'튀김류',
+			'국/찌개류',
+		];
 		dispatch(getFoodCategoryList_(foodCategoryList));
 	};
 };
