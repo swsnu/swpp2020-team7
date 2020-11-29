c140efe060bf5f20b098248d65051a1ced559039
eunjoo jung <lynn0506@snu.ac.kr>
2020-11-29 18:17:53 +0900

    fix: loading content

--- frontend/naengpa/src/components/Loading/Loading.tsx
+++ frontend/naengpa/src/components/Loading/Loading.tsx
@@ -6,11 +6,11 @@ import './Loading.scss';
 const Loading: React.FC = () => {
 	return (
 		<div id="loading">
-			<CircularProgress id="loading-bar" color="inherit" />
 			<LocalDiningIcon id="naengpa-logo-image" />
 			<div id="loading-content">
-				머신러닝 API를 이용해 재료 및 음식 카테고리를 추천 중입니다. 잠시만 기다려주세요!!!
+				머신러닝 API를 이용해 재료 및 음식 카테고리를 추천 중입니다.
 			</div>
+			<div id="loading-subcontent">잠시만 기다려주세요!!!</div>
 		</div>
 	);
 };
