14941a242784f317edde8d3a5bc185c0dddf6f4a
Eunsung Lim <unsungvegetable@gmail.com>
2020-12-03 18:45:40 +0900

    feat: configure seoul timezone

--- backend/naengpa/utils/aws_utils.py
+++ backend/naengpa/utils/aws_utils.py
@@ -1,5 +1,6 @@
 """utils for aws s3"""
 from datetime import datetime
+from django.utils import timezone
 import os
 import boto3
 
@@ -25,7 +26,8 @@ def get_filename_format(prefix, fid, user_id, file_idx, fname):
     fname: given input file name
     """
     current_time = datetime.now().strftime("%Y%m%d-%H%M%S-%f")
-    return "{}/{}/{}-{}-{}{}".format(prefix, fid, user_id, current_time, file_idx, os.path.splitext(fname)[1])
+    current_time_aware = timezone.make_aware(current_time)
+    return "{}/{}/{}-{}-{}{}".format(prefix, fid, user_id, current_time_aware, file_idx, os.path.splitext(fname)[1])
 
 
 def upload_images(files, prefix, feed_id, user_id):
