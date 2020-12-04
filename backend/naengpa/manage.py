f4aaf6866a7775bc2b99ccf6006aff802b9a778a
Eunsung Lim <unsungvegetable@gmail.com>
2020-12-04 23:31:47 +0900

    fix: fixed local/production naming to debug/deploy

--- backend/naengpa/manage.py
+++ backend/naengpa/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """main function"""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naengpa.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naengpa.settings.debug')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
