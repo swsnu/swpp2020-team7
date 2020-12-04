687fb20638ca542ae2873f3ea019598f28972a34
Eunsung Lim <unsungvegetable@gmail.com>
2020-12-05 01:16:04 +0900

    feat: add csrf trusted origins

--- backend/naengpa/naengpa/settings/deploy.py
+++ backend/naengpa/naengpa/settings/deploy.py
@@ -10,3 +10,5 @@ AWS_EC2_ELASTIC_IP = get_env_value('AWS_EC2_ELASTIC_IP')
 AWS_EC2_DNS_HOST_NAME = get_env_value('AWS_EC2_DNS_HOST_NAME')
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
                  AWS_EC2_ELASTIC_IP, 'naengpa.shop', AWS_EC2_DNS_HOST_NAME]
+CSRF_TRUSTED_ORIGINS = ['127.0.0.1', 'localhost',
+                        AWS_EC2_ELASTIC_IP, 'naengpa.shop', AWS_EC2_DNS_HOST_NAME]
