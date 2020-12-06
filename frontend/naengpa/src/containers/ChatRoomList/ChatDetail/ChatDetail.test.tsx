3eb005d83235cd29ad9661cc9a38745a8fa0e340
Eunsung Lim <unsungvegetable@gmail.com>
2020-12-06 21:54:52 +0900

    feat: implemented tests

--- frontend/naengpa/src/containers/ChatRoomList/ChatDetail/ChatDetail.test.tsx
+++ frontend/naengpa/src/containers/ChatRoomList/ChatDetail/ChatDetail.test.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { createMemoryHistory } from 'history';
+import thunk from 'redux-thunk';
 import ChatDetail from './ChatDetail';
+
+
+const middlewares = [thunk];
+const store = configureStore(middlewares);
+
+const mockChatRoom = {
+	id: "1",
+	lastChat: "hi",
+	member: "me",
+	updatedAt: "00",
+	chatCount: 1,
+};
+const stubInitialState = {
+	user: {
+		user: {
+			id: 'c2c13da9-5dcd-44a7-9cb6-92bbcdcf3f55',
+			name: '테스트',
+			username: 'test',
+			email: 'test@snu.ac.kr',
+			dateOfBirth: '20201112',
+			region: {
+				name: "관악구 대학동",
+			},
+		},
+		chatRoom: mockChatRoom,
+	},
+};
+const mockStore = store(stubInitialState);
+const history = createMemoryHistory({ initialEntries: ['/'] });
 
 describe('ChatDetail', () => {
 	let chatDetail: any;
 
 	beforeEach(() => {
-		chatDetail = <ChatDetail />;
+		chatDetail = (
+			<Provider store={mockStore}>
+				<ChatDetail history={history} />
+			</Provider>
+		);
 	});
 	afterEach(() => {
 		jest.clearAllMocks();
--- frontend/naengpa/src/containers/RegionalSetting/RegionalSetting.test.tsx
+++ frontend/naengpa/src/containers/RegionalSetting/RegionalSetting.test.tsx
@@ -1,13 +1,68 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { history } from '../../store/store';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+import { createMemoryHistory } from 'history';
 import RegionalSetting from './RegionalSetting';
+
+jest.mock('../../utils/kakao', () =>
+	jest.fn((props) => <div {...props} className="kakaoMap" />),
+);
+
+const middleware = [thunk];
+const store = configureStore(middleware);
+
+const history = createMemoryHistory({ initialEntries: ['/'] });
+
+const mockRegionList = [
+	{
+		"id": 4,
+		"name": "종로구 청운효자동",
+		"location": {
+			"latitude": 37.5841161738354,
+			"longitude": 126.97064969123
+		}
+	},
+	{
+		"id": 5,
+		"name": "종로구 사직동",
+		"location": {
+			"latitude": 37.5761869658796,
+			"longitude": 126.968846056089
+		}
+	},
+];
+const stubInitialState = {
+	user: {
+		saved_user: {
+			id: 'c2c13da9-5dcd-44a7-9cb6-92bbcdcf3f55',
+			username: 'test',
+			password: 'test',
+			email: 'test@snu.ac.kr',
+			name: '테스트',
+			dateOfBirth: '20201112',
+			region: {
+				name: "관악구 대학동"
+			},
+		},
+	},
+	region: {
+		regionList: mockRegionList,
+	},
+};
+const mockStore = store(stubInitialState);
 
 describe('RegionalSetting', () => {
 	let regionalSetting: any;
 
 	beforeEach(() => {
-		regionalSetting = <RegionalSetting history={history} />;
+		regionalSetting = (
+			<Provider store={mockStore}>
+				<RegionalSetting history={history} />;
+			</Provider>
+		);
+		
 	});
 	afterEach(() => {
 		jest.clearAllMocks();
@@ -16,22 +71,22 @@ describe('RegionalSetting', () => {
 		jest.restoreAllMocks();
 	});
 
-	// it('RegionalSetting renders without crashing', () => {
-	// 	const component = shallow(regionalSetting);
-	// 	expect(component.find('#regional-setting').length).toBe(1);
-	// });
+	it('RegionalSetting renders without crashing', () => {
+		const component = mount(regionalSetting);
+		expect(component.find('#regional-setting').length).toBe(1);
+	});
 
-	// it('should work well with the region search input', () => {
-	// 	const component = shallow(regionalSetting);
-	// 	expect(component.find('#regional-setting').length).toBe(1);
-	// 	const regionSearchInput = component.find('#region-search-input').find('input');
-	// 	// regionSearchInput.simulate('change', { target: { value: '봉천동' } });
-	// });
+	it('should work well with the region search input', () => {
+		const component = mount(regionalSetting);
+		expect(component.find('#regional-setting').length).toBe(1);
+		const regionSearchInput = component.find('#region-search-input').find('input');
+		// regionSearchInput.simulate('change', { target: { value: '봉천동' } });
+	});
 
-	// it('should work well with the region confirm button', () => {
-	// 	const component = shallow(regionalSetting);
-	// 	expect(component.find('#regional-setting').length).toBe(1);
-	// 	const regionSearchInput = component.find('#confirm-button');
-	// 	regionSearchInput.simulate('click');
-	// });
+	it('should work well with the region confirm button', () => {
+		const component = mount(regionalSetting);
+		expect(component.find('#regional-setting').length).toBe(1);
+		const regionSearchInput = component.find('#confirm-button');
+		regionSearchInput.simulate('click');
+	});
 });
