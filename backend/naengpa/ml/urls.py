a8dae37233de1066f97341259cfc6baae6334850
eunjoo jung <lynn0506@snu.ac.kr>
2020-12-06 18:43:53 +0900

    fix: image load error

--- backend/naengpa/ml/urls.py
+++ backend/naengpa/ml/urls.py
@@ -1,7 +1,9 @@
 """urls for ingredient"""
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from ml import views
 
 urlpatterns = [
     path('', views.extract_ml_feature, name='extract_ml_feature'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
