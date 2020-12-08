0c38e5dec1432a6d9916bb1560bf35d665a59674
snusun <0515hwang@snu.ac.kr>
2020-12-08 15:55:33 +0900

    env file and aws_utils modified

--- backend/naengpa/naengpa/settings/env.py
+++ backend/naengpa/naengpa/settings/env.py
@@ -1,4 +1,3 @@
-
 import os
 from dotenv import load_dotenv
 from django.core.exceptions import ImproperlyConfigured
@@ -18,8 +17,8 @@ def get_env_value(var_name):
 # AWS S3 BUCKET
 S3_URL = get_env_value("S3_URL")
 DEFAULT_FILE_STORAGE = get_env_value("DEFAULT_FILE_STORAGE")
-AWS_ACCESS_KEY_ID = get_env_value("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = get_env_value("AWS_SECRET_ACCESS_KEY")
+AWS_S3_ACCESS_KEY_ID = get_env_value("AWS_S3_ACCESS_KEY_ID")
+AWS_S3_SECRET_ACCESS_KEY = get_env_value("AWS_S3_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = get_env_value("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = get_env_value("AWS_S3_REGION_NAME")
 AWS_S3_FILE_OVERWRITE = get_env_value("AWS_S3_REGION_NAME")
