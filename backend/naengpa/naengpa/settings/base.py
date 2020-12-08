d0ce3c8061775fff14b188788f67c3f2a0c68e37
eunjoo jung <lynn0506@snu.ac.kr>
2020-12-09 07:47:05 +0900

    fix: Korean Time setting

--- backend/naengpa/naengpa/settings/base.py
+++ backend/naengpa/naengpa/settings/base.py
@@ -128,7 +128,8 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+# Korean Time Applied
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/assets/'
