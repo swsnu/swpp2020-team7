f31cf1460892880e4fe63ef757ee93cbb633b2fe
eunjoo jung <lynn0506@snu.ac.kr>
2020-12-08 19:28:24 +0900

    refactor: get list in advance

--- frontend/naengpa/src/containers/MyFridge/MyFridge.tsx
+++ frontend/naengpa/src/containers/MyFridge/MyFridge.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch }  from 'react-redux';
 import { History } from 'history';
 import { Grid } from '@material-ui/core';
 import TodayIngredient from '../../components/TodayIngredient/TodayIngredient';
@@ -7,6 +8,7 @@ import TodayRecipe from '../../components/TodayRecipe/TodayRecipe';
 import TodayStar from '../../components/TodayStar/TodayStar';
 import Fridge from '../Fridge/Fridge';
 import Footer from '../../components/Footer/Footer';
+import { getRecipeList, getFoodCategoryList } from '../../store/actions/index';
 import './MyFridge.scss';
 
 interface MyFridgeProps {
@@ -14,6 +16,13 @@ interface MyFridgeProps {
 }
 
 const MyFridge: React.FC<MyFridgeProps> = ({ history }) => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getFoodCategoryList());
+		dispatch(getRecipeList("", "created_at", "전체", 1));	
+	}, []);
+
 	return (
 		<div id="my-fridge-page">
 			<Grid id="my-fridge" container spacing={5}>
