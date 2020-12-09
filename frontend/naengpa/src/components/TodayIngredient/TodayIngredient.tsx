63175dab96063ad8efbcad99e80f4e73b6183d97
Jane Shin <sjane0516@snu.ac.kr>
2020-12-10 02:13:59 +0900

    added TodayIngredient Tutorial

--- frontend/naengpa/src/components/TodayIngredient/TodayIngredient.tsx
+++ frontend/naengpa/src/components/TodayIngredient/TodayIngredient.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Box, Grid } from '@material-ui/core';
+import { Button, Box, Grid, Collapse } from '@material-ui/core';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import Alert from '@material-ui/lab/Alert';
 import { toggleTodayIngredient, getFridge } from '../../store/actions/index';
 import { AppState } from '../../store/store';
 import './TodayIngredient.scss';
@@ -9,6 +11,7 @@ const TodayIngredient: React.FC = () => {
 	const dispatch = useDispatch();
 	const ingredientList = useSelector((state: AppState) => state.fridge.ingredientList);
 	const user = useSelector((state: AppState) => state.user.user);
+	const [alert, setAlert] = useState(false);
 	const todays_ingredient = ingredientList.filter(
 		(ingredient) => ingredient.isTodayIngredient === true,
 	);
@@ -45,6 +48,16 @@ const TodayIngredient: React.FC = () => {
 				</Grid>
 				<Grid item xs>
 					<Box id="today-ingredient-contents">{todays_ingredient_contents}</Box>
+					<HelpOutlineIcon
+						id="help-today-ingredient"
+						onMouseOver={() => setAlert(true)}
+						onMouseLeave={() => setAlert(false)}
+						onFocus={() => setAlert(true)} />
+					<Collapse in={alert}>
+						<Alert id="help-today-ingredient-alert" icon={false}>
+							냉장고 속 재료를 클릭하여 오늘의 재료에 추가해보세요! 추가한 재료를 기반으로 레시피를 추천해드립니다.
+						</Alert>
+					</Collapse>
 				</Grid>
 			</Grid>
 		</div>
