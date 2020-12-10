b10705adb2bd51101cb79f54d713bf8c334984f6
Eunsung Lim <unsungvegetable@gmail.com>
2020-12-11 00:55:14 +0900

    fix: fixed wrong ** usage

--- backend/naengpa/user/models.py
+++ backend/naengpa/user/models.py
@@ -59,12 +59,12 @@ class User(AbstractUser):
 
     def save(self, *args, **kwargs):
         cache.delete_many(
-            'users', **[f'recipe:{id}' for id in self.recipes.values_list('id', flat=True)])
+            'users', *map(lambda id: f'recipe:{id}', self.recipes.values_list('id', flat=True)))
         super().save(*args, **kwargs)
 
     def delete(self, *args, **kwargs):
         cache.delete_many(
-            'users', **[f'recipe:{id}' for id in self.recipes.values_list('id', flat=True)])
+            'users', *map(lambda id: f'recipe:{id}', self.recipes.values_list('id', flat=True)))
         super().delete(*args, **kwargs)
 
 
