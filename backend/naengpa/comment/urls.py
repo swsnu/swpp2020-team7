fbc1ec3a34ad7554fb09e6dc5d60670b484e1eef
Eunsung Lim <unsungvegetable@gmail.com>
2020-12-13 01:54:14 +0900

    feat: add comment like, edit

--- backend/naengpa/comment/urls.py
+++ backend/naengpa/comment/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.comment_list, name='comment_list'),
     path('<int:cid>/', views.comment_info, name='comment_info'),
+    path('<int:id>/like/', views.comment_like, name='comment_like'),
 ]
