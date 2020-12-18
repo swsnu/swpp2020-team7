1408bb52862f068e0ea506fb9f6723d08e64a37a
Eunsung Lim <unsungvegetable@gmail.com>
2020-12-18 17:45:18 +0900

    test: recipe list

--- frontend/naengpa/src/containers/RecipeList/RecipeList.test.tsx
+++ frontend/naengpa/src/containers/RecipeList/RecipeList.test.tsx
@@ -9,6 +9,16 @@ import RecipeList from './RecipeList';
 import * as recipeActionCreators from '../../store/actions/recipe';
 import waitForComponentToPaint from '../../../test-utils/waitForComponentToPaint';
 
+jest.mock('@material-ui/core/Select', () =>
+	jest.fn((props) => <div {...props} className="spySelect" />),
+);
+jest.mock('@material-ui/core/MenuItem', () =>
+	jest.fn((props) => <div {...props} className="spyMenuItem" />),
+);
+jest.mock('@material-ui/lab/Pagination', () =>
+	jest.fn((props) => <div {...props} className="spyPagination" />),
+);
+
 const middlewares = [thunk];
 const store = configureStore(middlewares);
 
@@ -54,13 +64,8 @@ const stubInitialState = {
 				foodCategory: '밥류',
 				ingredients: ['돼지고기', '고추장'],
 			},
-			{ id: 3 },
-			{ id: 4 },
-			{ id: 5 },
-			{ id: 6 },
-			{ id: 7 },
-			{ id: 8 },
 		],
+		lastPageIndex: 2,
 	},
 	foodCategory: {
 		foodCategoryList: [
@@ -75,7 +80,7 @@ jest.mock('../../components/Recipe/Recipe', () =>
 );
 
 describe('RecipeList', () => {
-	let getRecipeList: any;
+	let recipeList: any;
 	let spyGetRecipeList: any;
 	let spyHistoryPush: any;
 
@@ -86,7 +91,7 @@ describe('RecipeList', () => {
 		}));
 
 		act(() => {
-			getRecipeList = (
+			recipeList = (
 				<Provider store={mockStore}>
 					<RecipeList history={history} />
 				</Provider>
@@ -109,14 +114,14 @@ describe('RecipeList', () => {
 	});
 
 	it('RecipeList renders without crashing', async () => {
-		const component = mount(getRecipeList);
+		const component = mount(recipeList);
 		await waitForComponentToPaint(component);
 		expect(component.find('#recipe-list').length).toBe(1);
 		expect(spyGetRecipeList).toBeCalledTimes(1);
 	});
 
 	it('should click create Recipe button correctly', async () => {
-		const component = mount(getRecipeList);
+		const component = mount(recipeList);
 		await waitForComponentToPaint(component);
 		const wrapper = component.find('#create-recipe-button');
 		wrapper.find('button').at(0).simulate('click');
@@ -124,30 +129,108 @@ describe('RecipeList', () => {
 	});
 
 	it('should check if pagination works', async () => {
-		const component = mount(getRecipeList);
+		const component = mount(recipeList);
 		await waitForComponentToPaint(component);
 		const pageButton = component.find('#recipe-list-page');
 		pageButton.at(0).simulate('click');
 	});
 
-	it('should check if search recipe works', async () => {
-		const component = mount(getRecipeList);
+	it('handles change filters correctly', async () => {
+		const component = mount(recipeList);
 		await waitForComponentToPaint(component);
+
+		const optionsFilter = component.find('div#recipe-list-buttons').find('button');
+		act(() => {
+			optionsFilter.at(0).simulate('click');
+			optionsFilter.at(1).simulate('click');
+			optionsFilter.at(2).simulate('click');
+		});
+		await waitForComponentToPaint(component);
+
+		expect(spyGetRecipeList).toBeCalled();
+	});
+
+	it('handles change category correctly', async () => {
+		const component = mount(recipeList);
+		await waitForComponentToPaint(component);
+
+		const searchSelect = component.find('#recipe-search-select').first();
+		act(() => {
+			searchSelect.simulate('change', {
+				preventDefault: jest.fn(),
+				target: { value: '밥류' },
+			});
+		});
+		await waitForComponentToPaint(component);
+		expect(spyGetRecipeList).toBeCalled();
+
+		act(() => {
+			const searchSelectItem = searchSelect.find('.spyMenuItem').first();
+			searchSelectItem.simulate('click');
+		});
+		await waitForComponentToPaint(component);
+		expect(spyGetRecipeList).toBeCalled();
+
+		act(() => {
+			const searchSelectItem = searchSelect.find('.spyMenuItem').at(1);
+			searchSelectItem.simulate('click');
+		});
+		await waitForComponentToPaint(component);
+		expect(spyGetRecipeList).toBeCalled();
+
+		act(() => {
+			const searchSelectItem = searchSelect.find('.spyMenuItem').at(2);
+			searchSelectItem.simulate('click');
+		});
+		await waitForComponentToPaint(component);
+		expect(spyGetRecipeList).toBeCalled();
+	});
+
+	it('should check if search recipe works', async () => {
+		const component = mount(recipeList);
+		await waitForComponentToPaint(component);
+
 		const recipeSearchInput = component.find('#recipe-search-input').find('input');
-		recipeSearchInput.simulate('change', { target: { value: '레시피' } });
-		const recipeSearchCategory = component.find('#recipe-search-select').find('div').at(0);
-		recipeSearchCategory.simulate('click');
-		recipeSearchCategory.simulate('change', { target: { value: '한식' } });
-		recipeSearchInput.simulate('keyDown', { key: '' });
-		recipeSearchInput.simulate('keyDown', { key: 'Enter' });
+		act(() => {
+			recipeSearchInput.simulate('change', { target: { value: '레시피' } });
+			const recipeSearchCategory = component.find('#recipe-search-select').find('div').at(0);
+			recipeSearchCategory.simulate('click');
+			recipeSearchCategory.simulate('change', { target: { value: '한식' } });
+			recipeSearchInput.simulate('keyDown', { key: '' });
+			recipeSearchInput.simulate('keyDown', { key: 'Enter' });
+		});
+		await waitForComponentToPaint(component);
+
+		expect(spyGetRecipeList).toBeCalled();
 	});
 
 	it('should click buttons correctly', async () => {
-		const component = mount(getRecipeList);
+		const component = mount(recipeList);
 		await waitForComponentToPaint(component);
+
 		const filterButton = component.find('#filter-button-true');
-		filterButton.at(0).simulate('click');
-		const filterFalseButton = component.find('#filter-button-false');
-		filterFalseButton.at(0).simulate('click');
+		act(() => {
+			filterButton.at(0).simulate('click');
+			const filterFalseButton = component.find('#filter-button-false');
+			filterFalseButton.at(0).simulate('click');
+		});
+		await waitForComponentToPaint(component);
+
+		expect(spyGetRecipeList).toBeCalled();
+	});
+
+	it('should change page correctly', async () => {
+		const component = mount(recipeList);
+		await waitForComponentToPaint(component);
+
+		act(() => {
+			component.find('#recipe-list-page').first().prop('onChange')(
+				{ preventDefault: jest.fn() },
+				2,
+			);
+		});
+		await waitForComponentToPaint(component);
+
+		expect(spyGetRecipeList).toBeCalled();
 	});
 });
